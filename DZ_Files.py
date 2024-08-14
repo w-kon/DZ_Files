fff4ce1589e9acc34344629392a3f04d1dcdf580
w-kon <wkon@yandex.ru>
2024-08-14 14:41:44 +0600

    dictionary_list

--- DZ_Files.py
+++ DZ_Files.py
@@ -1,8 +1,8 @@
 
-cook_book = {}
-title = ['ingredient_name', 'quantity', 'measure']
-with open('recipes.txt', 'r', encoding='utf-8') as file:
-    for line in file:
+def dictionary_list(list_recipes):
+    cook_book = {}
+    title = ['ingredient_name', 'quantity', 'measure']
+    for line in list_recipes:
         line = line.strip().split(' | ')
         if len(''.join(line)) > 1:
             if len(line) == 1 and not ''.join(line).isdigit(): 
@@ -10,5 +10,11 @@ with open('recipes.txt', 'r', encoding='utf-8') as file:
                 cook_book[key] = []
             elif not ''.join(line).isdigit():
                 cook_book[key].append(dict(zip(title, line)))
-                
-print(cook_book)
+    return cook_book
+
+with open('recipes.txt', 'r', encoding='utf-8') as file:
+    print(dictionary_list(file))
+
+
+
+
